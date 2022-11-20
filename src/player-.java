19277c8603cc4d1ec59784ca555391d104b4cbd9
jingruidu <115423997+jingruidu@users.noreply.github.com>
2022-11-20 17:04:26 +0100

    Add files via upload

---
+++ src/player-.java
@@ -0,0 +1,32 @@
+
+import processing.core.PApplet;
+
+
+public class player{
+    public input getplayername(){
+        return input
+    }
+
+    PApplet parent;
+
+    int playerscore=0;
+    string name;
+    int numberofplayer;
+
+    player(int newer, PApplet p){
+       parent=p;
+       numberofplayer=newer-1;
+       name="Player"+newer;
+    }
+
+        
+    void addscore( int new score) {
+    playerscore += addscore;   
+    }
+    
+    
+}
+
+}
+
+                    
