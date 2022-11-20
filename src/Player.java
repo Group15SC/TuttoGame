e9d51fcc69b21d4fed8ce9c200a6e1ce356d7e42
rChenfeiXiong <113922300+rChenfeiXiong@users.noreply.github.com>
2022-11-20 17:18:31 +0100

    change Player

--- src/Player.java
+++ src/Player.java
@@ -1,31 +1,46 @@
 
-import processing.core.PApplet;
+//import processing.core.PApplet;
 
 
-public class Player{
-    public input getplayername(){
-        return input
+public class Player {
+
+    public String name;
+    public int score;
+
+    public Player(String name, int score) {
+        this.name = name;
+        this.score = score;
     }
 
-    PApplet parent;
-
-    int playerscore=0;
-    string name;
-    int numberofplayer;
-
-    player(int newer, PApplet p){
-       parent=p;
-       numberofplayer=newer-1;
-       name="Player"+newer;
+    public int getScore() {
+        return score;
     }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+//    public input getplayername(){
+//        return input
+//    }
+//
+//    PApplet parent;
+//
+//    int playerscore=0;
+//    string name;
+//    int numberofplayer;
+
+//    player(int newer, PApplet p){
+//       parent=p;
+//       numberofplayer=newer-1;
+//       name="Player"+newer;
+//    }
 
         
-    void addscore( int new score) {
-    playerscore += addscore;   
-    }
-    
-    
-}
+//    void addscore( int new score) {
+//    playerscore += addscore;
+//    }
+
 
 }
 
